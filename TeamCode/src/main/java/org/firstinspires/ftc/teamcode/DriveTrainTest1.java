bd10a0688e1b65f31e7ea2cbdd19f9bee0de7b7a
Eli Leser <lesereli000@students.centergrove.k12.in.us>
2018-12-04 20:03:56 -0500

    bug fixes 1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrainTest1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrainTest1.java
@@ -11,15 +11,40 @@ class DriveTrainTest1 extends LinearOpMode {
     public void runOpMode() {
         double left;
         double right;
+        double extend;
+        double MCSarm;
 
         DcMotor leftDrive = hardwareMap.dcMotor.get("mLeft");
         DcMotor rightDrive = hardwareMap.dcMotor.get("mRight");
-
-        leftDrive.setDirection(DcMotor.Direction.REVERSE);
-        rightDrive.setDirection(DcMotor.Direction.FORWARD);
+        DcMotor mineralArm = hardwareMap.dcMotor.get("mArm");
+        DcMotor mineralBox = hardwareMap.dcMotor.get("mBox");
+        DcMotor liftArm = hardwareMap.dcMotor.get("mLift");
+        DcMotor extensionMotor = hardwareMap.dcMotor.get("mExtend");
 
         leftDrive.setPower(0);
         rightDrive.setPower(0);
+        liftArm.setPower(0);
+        mineralArm.setPower(0);
+        mineralBox.setPower(0);
+        extensionMotor.setPower(0);
+
+        leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftDrive.setDirection(DcMotor.Direction.FORWARD);
+
+        rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightDrive.setDirection(DcMotor.Direction.FORWARD);
+
+        mineralArm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        mineralArm.setDirection(DcMotor.Direction.FORWARD);
+
+        mineralBox.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        mineralBox.setDirection(DcMotor.Direction.FORWARD);
+
+        liftArm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        liftArm.setDirection(DcMotor.Direction.FORWARD);
+
+        extensionMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        extensionMotor.setDirection(DcMotor.Direction.FORWARD);
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
@@ -27,14 +52,69 @@ class DriveTrainTest1 extends LinearOpMode {
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()){
 
-            // Run wheels in tank mode (note: The joystick goes negative when pushed forwards, so negate it)
+            // Run wheels in tank drive (note: The joystick goes negative when pushed forwards, so negate it)
             left = gamepad1.left_stick_y;
-            right = gamepad1.right_stick_x;
+            right = gamepad1.right_stick_y;
             leftDrive.setPower(left);
             rightDrive.setPower(right);
 
-            // Pause for 40 mS each cycle = update 25 times a second.
-            sleep(40);
+
+                // run until the end of the match (driver presses STOP)
+                while (opModeIsActive()) {
+
+                    // Run wheels in tank mode (note: The joystick goes negative when pushed forwards, so negate it)
+                    left = gamepad1.left_stick_y;
+                    right = gamepad1.left_stick_x;
+                    leftDrive.setPower(left);
+                    rightDrive.setPower(right);
+
+                    if (gamepad2.right_bumper = true) {
+                        extensionMotor.setDirection(DcMotor.Direction.FORWARD);
+                        mineralArm.setDirection(DcMotor.Direction.FORWARD);
+                        extensionMotor.setPower(.25);
+                        mineralArm.setPower(.5);
+                    } else {
+                        extensionMotor.setPower(0);
+                        mineralArm.setPower(0);
+                    }
+
+                    if (gamepad2.left_bumper = true) {
+                        extensionMotor.setDirection(DcMotor.Direction.REVERSE);
+                        mineralArm.setDirection(DcMotor.Direction.REVERSE);
+                        extensionMotor.setPower(.25);
+                        mineralArm.setPower(.5);
+                    } else {
+                        extensionMotor.setPower(0);
+                        mineralArm.setPower(0);
+                    }
+
+
+                    while (gamepad2.left_trigger > 0) {
+                        mineralBox.setDirection(DcMotor.Direction.FORWARD);
+                        mineralBox.setPower(1);
+                    }
+
+                    while (gamepad2.right_trigger > 0) {
+                        mineralBox.setDirection(DcMotor.Direction.REVERSE);
+                        mineralBox.setPower(1);
+                    }
+
+                    extend = gamepad2.right_stick_x;
+                    extensionMotor.setPower(extend);
+
+                    MCSarm = gamepad2.left_stick_y;
+                    mineralArm.setPower(MCSarm);
+
+                    if (gamepad1.dpad_down = true) {
+                        liftArm.setDirection(DcMotor.Direction.FORWARD);
+                        liftArm.setPower(1);
+                    }
+
+                    if (gamepad1.dpad_up = true) {
+                        liftArm.setDirection(DcMotor.Direction.REVERSE);
+                        liftArm.setPower(1);
+                    }
+                }
+            }
         }
     }
-}
